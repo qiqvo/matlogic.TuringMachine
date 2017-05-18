a4881f7e3995b251fb4bcb3044b242592ba5663e
qiqvo <xanderprix@gmail.com>
2017-05-18 08:53:26 +0300

    edit

--- matlogic.TuringMachine/Machine.h
+++ matlogic.TuringMachine/Machine.h
@@ -2,20 +2,27 @@
 #include "RuleStateData.h"
 #include <map>
 #include <fstream>
+
 using namespace std;
 
 using Rules = map<pair<State, Symbol>, Rule>;
 
 class Machine {
-private:
+public:
 	static const unsigned int len_of_tape = 100;
+private:
 	char input[len_of_tape];
 
+	unsigned int num_of_steps;
 	Rules rls;
 	State cur_state;
 	Data *cur_data;
+
+	void Add_Rule(const State st, const Symbol s, const Rule r) {
+		rls.insert({ { st, s }, r });
+	}
 public:
-	Machine() : cur_state(0) {
+	Machine() : cur_state(0), num_of_steps(0){
 		unsigned i = 0;
 		for (i; i < len_of_tape - 1; ++i) {
 			input[i] = static_cast<char>(Symbol::Lambda);
@@ -30,18 +37,10 @@ public:
 		}
 		cur_data = new Data(input);
 	}
-
-	void Add_Rule(const State st, const Symbol s, const Rule r) {
-		rls.insert({ {st, s}, r });
-	}
-	char* GO() {
-		while (cur_state != -1) {
-			cur_state = 
-				(*rls.find({ cur_state, cur_data->_symb }))
-				.second(*cur_data);
-		}
-		input[len_of_tape - 1] = '\0';
-		return input;
+	void SetRules(const char* f_name = "RULES");
+	char* GO(ofstream* out);
+	unsigned int NumberOfSteps() {
+		return num_of_steps;
 	}
 };
 
