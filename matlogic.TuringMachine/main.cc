4f478321058cba7f984edab95b31ca1d1528634d
qiqvo <xanderprix@gmail.com>
2017-05-18 08:55:07 +0300

    edited because of new func in Machine

--- matlogic.TuringMachine/main.cc
+++ matlogic.TuringMachine/main.cc
@@ -17,72 +17,29 @@ q1 -- end state
 R  -- move head in Right Direction
 */
 
-template <class Cont>
-Cont split(const string& s, const char del) {
-	std::stringstream ss;
-	ss.str(s);
-	std::string item;
-	Cont elems;
-
-	while (std::getline(ss, item, del)) {
-		if(!item.empty())
-			elems.push(item);
-	}
-	return elems;
-}
-
 int main() {
-	ifstream rules_file("RULES");
+	// std::ios::sync_with_stdio(false);
 	ifstream in_file("Input");
-	Machine Turing;
-	string tmp;
-	const char q = 'q';
-	const char terminal = '*';
+	ofstream out_file1("Output.MacOld");
+	ofstream out_file2("Output.MacNew");
 
-	int state_beg_num, state_end_num;
-	char init_sign, end_sign;
-	char direct;
-	
-	while (getline(rules_file, tmp)) {
-		auto tokens = split<queue<string>>(tmp, ' ');
+	Machine Test_old, Test_new;
+	Test_old.SetRules("RULES.standartMult");
+	Test_new.SetRules("RULES.NewMult");
 
-		if (tokens.front()[0] != q)
-			continue;
-		else
-			state_beg_num = stoi(tokens.front().substr(1));
-		tokens.pop(); // - q0
+	Test_old.GetInput(&in_file);
+	in_file.clear();
+	in_file.seekg(0, in_file.beg);
+	Test_new.GetInput(&in_file);
 
-		init_sign = tokens.front()[0];
-		tokens.pop(); // - |
-		tokens.pop(); // - ->
+	Test_old.GO(&out_file1);
+	Test_new.GO(&out_file2);
 
-		if (tokens.front()[0] != q)
-			continue;
-		else if (tokens.front()[1] == terminal)
-			state_end_num = -1;
-		else
-			state_end_num = stoi(tokens.front().substr(1));
-		tokens.pop(); // - q1
-
-		end_sign = tokens.front()[0];
-		tokens.pop(); // - o
-
-		if (tokens.empty())
-			direct = static_cast<char>(Direction::inPlace);
-		else {
-			direct = tokens.front()[0];
-			tokens.pop(); // - R
-		}
-		
-		Turing.Add_Rule(state_beg_num, static_cast<Symbol>(init_sign),
-		{ state_end_num, end_sign, direct });
-	}
-
-	Turing.GetInput(&in_file);
-	cout << Turing.GO() << endl;
-
-	rules_file.close();
 	in_file.close();
-	cin >> direct;
+	out_file1.close();
+	out_file2.close();
+	
+	int tmp;
+	cin >> tmp;
 	return 0;
 }
